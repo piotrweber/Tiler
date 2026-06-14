45ddb42808d0afcb057b0bf9b66023871aee847e
piotrweber <85232061+piotrweber@users.noreply.github.com>
2026-06-14 14:17:24 +0200

    Removed logic for hiding unlocked platforms

--- scenes/platform_rules.gd
+++ scenes/platform_rules.gd
@@ -8,10 +8,6 @@ class_name PlatformRules
 func on_tile_stepped_on(tile : BaseTile):
 	var platform : Dictionary = layer.platforms[tile.platform_id]
 
-	if layer.current_platform != tile.platform_id:
-		conceal_platform(layer.current_platform)
-		layer.current_platform = tile.platform_id
-
 	if platform.is_locked:
 		return
 
@@ -41,16 +37,6 @@ func lock_platform(platform_id : int):
 	count_locked_platforms()
 	
 
-func conceal_platform(id: int) -> void:
-	if id < 0:
-		return
-	var p: Dictionary = layer.platforms[id]
-	if p.is_locked:
-		return
-	p.stepped.clear()
-	for cell: Vector2i in p.cells:
-		var tile: BaseTile = layer.tiles_by_cell[cell]
-		tile.conceal()
 		
 func count_locked_platforms():
 	layer.locked_count += 1
